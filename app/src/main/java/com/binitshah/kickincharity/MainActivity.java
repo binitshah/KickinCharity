a756778e83c959abc857181e04dec0039f909067
bshah <bshah2016@gmail.com>
2016-10-23 03:31:31 -0400

    Onboarding Finished

--- app/src/main/java/com/binitshah/kickincharity/MainActivity.java
+++ app/src/main/java/com/binitshah/kickincharity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.binitshah.kickincharity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -13,10 +14,10 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntroFragment;
 
-public class MainActivity extends AppIntro {
+public class MainActivity extends AppIntro2 {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,24 +42,29 @@ public class MainActivity extends AppIntro {
         addSlide(AppIntroFragment.newInstance(
                 getResources().getString(R.string.slide2title),
                 getResources().getString(R.string.slide2description),
-                R.drawable.logo,
+                R.drawable.slide3,
                 ContextCompat.getColor(this, R.color.slide2color)));
 
         addSlide(AppIntroFragment.newInstance(
                 getResources().getString(R.string.slide3title),
                 getResources().getString(R.string.slide3description),
-                R.drawable.logo,
+                R.drawable.slide4,
                 ContextCompat.getColor(this, R.color.slide3color)));
 
         addSlide(AppIntroFragment.newInstance(
                 getResources().getString(R.string.slide4title),
                 getResources().getString(R.string.slide4description),
-                R.drawable.logo,
+                R.drawable.slide5,
                 ContextCompat.getColor(this, R.color.slide4color)));
 
+
+        addSlide(new LoginFragment());
         // OPTIONAL METHODS
         // Override bar/separator color.
-        setSeparatorColor(Color.parseColor("#002196F3"));
+        //setSeparatorColor(Color.parseColor("#002196F3"));
+
+        setDepthAnimation();
+        showStatusBar(false);
 
         // Hide Skip/Done button.
         showSkipButton(true);
@@ -70,12 +76,16 @@ public class MainActivity extends AppIntro {
     public void onSkipPressed(Fragment currentFragment) {
         super.onSkipPressed(currentFragment);
         // Do something when users tap on Skip button.
+        Intent intent = new Intent(this, ChairtyMapsActivity.class);
+        startActivity(intent);
     }
 
     @Override
     public void onDonePressed(Fragment currentFragment) {
         super.onDonePressed(currentFragment);
         // Do something when users tap on Done button.
+        Intent intent = new Intent(this, ChairtyMapsActivity.class);
+        startActivity(intent);
     }
 
     @Override
